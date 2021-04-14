7b5e8d6e8287c051e93f597e3c8eb3eb9af05cb9
rayk47 <raymond,kelly43@mail.dcu.ie>
2021-04-13 18:05:48 -0700

    fix(): filter email to only show new appointments

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import * as cron from 'node-cron';
 import { sendEmail } from "./email/Gmail";
 import { getListofStoresById, getStoresWithVaccinesAvailableWithinDistance } from './filter/FilterStores';
 import dotenv from 'dotenv';
+import { IStoreData } from './vaccineSpotter/VaccineSpotter';
 dotenv.config();
 
 //These should all be moved into their own class...if this was an API service they would be passed in an api call and stored in a db
@@ -27,10 +28,20 @@ function start(){
         const currentListOfStoresWithAppt = getListofStoresById(storesWithVaccines);
         //Send a notification only if there is a new store added to the list
         if(currentListOfStoresWithAppt.length && currentListOfStoresWithAppt.some(r=> !previousListOfStoresWithAppt.includes(r))) {
+            storesWithVaccines = storesWithVaccines.filter(store => {
+                return isNewlyAdded(store, previousListOfStoresWithAppt);
+            });
             sendEmail(STATE, ZIPCODE, RADIUS, storesWithVaccines);   
             previousListOfStoresWithAppt = currentListOfStoresWithAppt;      
         } else {
             console.log("No new appointments found");
         }
     });
+}
+
+function isNewlyAdded(store: IStoreData, previousListOfStoresWithAppt:number[]): boolean {
+    if(previousListOfStoresWithAppt.includes(store.properties.id)){
+        return false;
+    }
+    return true;
 }
